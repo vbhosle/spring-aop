7304651d1ec43ffab42488a8a243059c35c090ce
Viraj Bhosle <bhosleviraj@gmail.com>
2018-10-04 13:38:25 +0530

    Spring Tutorial 34 - AOP XML configuration

--- src/main/java/org/javabrains/koushik/aspect/LoggingAspect.java
+++ src/main/java/org/javabrains/koushik/aspect/LoggingAspect.java
@@ -13,32 +13,6 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class LoggingAspect {
 
-	@Before("allCircleMethods()")
-	public void loggingAdvice(JoinPoint joinPoint) {
-		System.out.println(joinPoint.toString());
-	}
-	
-	@Before("args(name)")
-	public void stringArgumentMethods(String name) {
-		System.out.println("Before method with String argument called, arg:" + name);
-	}
-	
-	@After("args(name)")
-	public void stringArgumentMethodsAfter(String name) {
-		System.out.println("After method with String argument called, arg:" + name);
-	}
-	
-	@AfterReturning(pointcut="args(name)", returning="returnString")
-	public void stringArgumentMethodsAfterReturning(String name, String returnString) {
-		System.out.println("After returning from method with String argument, arg:" + name + ", returned:"+returnString);
-	}
-	
-	@AfterThrowing(pointcut="args(name)", throwing="ex")
-	public void stringArgumentMethodsAfterThrowing(String name, Exception ex) {
-		System.out.println("After method with String argument throw exception, arg:" + name +", Exception:"+ ex);
-	}
-	
-	@Around("@annotation(org.javabrains.koushik.aspect.Loggable)")
 	public Object myAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 		//actual method execution, so we can skip the execution!
 		System.out.println("Around: Before");
@@ -47,9 +21,4 @@ public class LoggingAspect {
 		return returnType;
 	}
 	
-	@Pointcut("execution(* get*())")
-	public void allGetters() {}
-	
-	@Pointcut("within(org.javabrains.koushik.model.Circle)")
-	public void allCircleMethods() {}
 }
